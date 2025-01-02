9b53e1223f478cb844446e73c4fccd55d21bc322
MOSSI-wq <sidialimoslim0@gmail.com>
2025-01-02 14:01:43 +0100

    config

--- src/main/java/com/microservice/commande/CommandeApplication.java
+++ src/main/java/com/microservice/commande/CommandeApplication.java
@@ -2,8 +2,12 @@ package com.microservice.commande;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigurationProperties
 public class CommandeApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommandeApplication.class, args);
